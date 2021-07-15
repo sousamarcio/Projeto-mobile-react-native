179a774bad3b62c541c0795306de343f4c0cc3c0
Marcio Sousa <sousa-marcio@live.com>
2021-07-15 01:17:59 -0300

    Adição da tela de carrinho, componente tela padrão e total do carrinho

--- src/telas/Carrinho/index.js
+++ src/telas/Carrinho/index.js
@@ -4,30 +4,37 @@ import { FlatList } from 'react-native';
 
 import Item from './Item';
 import TelaPadrao from '../../componentes/TelaPadrao';
+import StatusCarrinho from '../../componentes/StatusCarrinho';
 
 const servicos = [
     {
         id: 1,
         nome: "Banho",
         preco: 79.9,
-        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos."
+        descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos.",
+        quantidade: 1
     },
     {
         id: 2,
         nome: "Vacina V4",
         preco: 89.9,
-        descricao: "Uma dose da vacina V4, Seu gato precisa de duas."
+        descricao: "Uma dose da vacina V4, Seu gato precisa de duas.",
+        quantidade: 2
     },
     {
         id: 3,
         nome: "Vacina Antirrábica",
         preco: 99.9,
-        descricao: "Uma dose da vacina antirrábica, Seu gato precisa de uma por ano."
+        descricao: "Uma dose da vacina antirrábica, Seu gato precisa de uma por ano.",
+        quantidade: 1
     }
 ];
 
-export default function Servicos() {
+export default function Carrinho() {
+    const total = servicos.reduce((soma, { preco, quantidade }) => soma + (preco * quantidade), 0);
+
     return <TelaPadrao>
+        <StatusCarrinho total={total} />
         <FlatList
             data={servicos}
             renderItem={({ item }) => <Item {...item} />}
