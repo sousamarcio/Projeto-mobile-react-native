f0bc106bcc598ba1660aad63eb9b805cbd55bf7f
Marcio Sousa <sousa-marcio@live.com>
2021-07-15 03:46:37 -0300

    Adicionando reactotron no projeto para logs

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
+import Reactotron from 'reactotron-react-native';
 
 import Rotas from './src/rotas';
 import TelaPadrao from './src/componentes/TelaPadrao';
+
+Reactotron.configure().useReactNative().connect();
 
 export default function App() {
   return <TelaPadrao><Rotas /></TelaPadrao>;
